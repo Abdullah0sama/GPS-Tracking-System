08a9fac734931ea7bfaef0c4949ff58ccd1794c4
mohamedali1801534 <1801534@eng.asu.edu.eg>
2021-06-05 21:58:53 +0200

    adding data type string

--- LCD_init.h
+++ LCD_init.h
@@ -1,12 +1,30 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
-
+#include <string.h>
 
 void delay_milli(int n);
 void delay_micro(int n);
 void LCD_Cmd(unsigned char command);
 void LCD_Data(unsigned char data);
 void LCD_init(void);
+void Lcd_String(unsigned char* data);
+
+#define clear_display     0x01
+#define returnHome        0x02
+#define moveCursorRight   0x06
+#define moveCursorLeft    0x08
+#define shiftDisplayRight 0x1C
+#define shiftDisplayLeft  0x18
+#define cursorBlink       0x0F
+#define cursorOff         0x0C
+#define cursorOn          0x0E
+#define Function_set_4bit 0x28
+#define Function_set_8bit 0x38
+#define Entry_mode        0x06
+#define Function_8_bit    0x32
+#define Set5x7FontSize    0x20
+#define FirstRow          0x80
+
 
 void LCD_init(void){
  //PORT(A7,A6,A5),PORTB clock
@@ -61,17 +79,25 @@ void LCD_Cmd(unsigned char command) {
 
      GPIO_PORTB_DATA_R =data;
      GPIO_PORTA_DATA_R =0x20; //RS=1, E=0,RW=0
+	 
      delay_milli(50);
      delay_milli(50);
 
-    GPIO_PORTA_DATA_R = 0x80 | 0x20;
-    delay_micro(50);
-    GPIO_PORTA_DATA_R  &= ~ 0x80;
-    delay_micro(50);
+     GPIO_PORTA_DATA_R = 0x80 | 0x20;
+     delay_micro(50);
+     GPIO_PORTA_DATA_R  &= ~ 0x80;
+     delay_micro(50);
 
     }
 	// For Delay
-
+void Lcd_String(unsigned char* data)
+{
+	int i;
+	for (i = 0; data[i] != 0; i++)
+	{
+		Lcd_Data(data[i]);
+	}
+}
 void delay_milli(int n){
 int i,j;
 for(i=0;i<n;i++){
