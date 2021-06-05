6d75f6b2f53062a58e0aea34a4e19ed6c1bc480b
Ahmedkhaled2221 <85367025+Ahmedkhaled2221@users.noreply.github.com>
2021-06-06 01:19:17 +0200

    Add files via upload

---
+++ uart_init.h
@@ -0,0 +1,21 @@
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void UART_Init(void){
+SYSCTL_RCGCUART_R |=0x0004; //enable uart4 and Port D
+SYSCTL_RCGCGPIO_R |=0x0008;
+UART2_CTL_R &=~0x0001; //diable uart uartctl
+
+UART2_IBRD_R = 0x0208; //or 104 
+UART2_FBRD_R = 0x35 ;   //baudrate
+UART2_LCRH_R = 0x0070;      
+UART2_CTL_R |= 0x0001;  //enable uartctl
+
+GPIO_PORTD_AFSEL_R |= 0xC0;
+GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R &0x00FFFFFF)+0x11000000 ;
+GPIO_PORTD_DEN_R |= 0xC0;     
+GPIO_PORTD_AMSEL_R &=~ 0XC0;       
+
+}
