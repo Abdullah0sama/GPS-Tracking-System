196e865a14197294af47e357634a9a3d68a40335
3omdawy160 <85120725+3omdawy160@users.noreply.github.com>
2021-06-06 02:33:55 +0200

    Update GetPosition.h

--- GetPosition.h
+++ GetPosition.h
@@ -1,3 +1,6 @@
+#include "Reading Parameters from GPS 2.h"
+#include "ReadingFromGPS.h"
+#include "uart4_init.h"
 struct position{
     double lat;
     double lon;
@@ -5,7 +8,7 @@ struct position{
 typedef struct position pos;
 pos getPosition(){
     pos temp;
-    temp.lat = 1;
-    temp.lon = 1;
+    temp.lat = readThis1();
+    temp.lon = readThis2();
     return temp;
 }
