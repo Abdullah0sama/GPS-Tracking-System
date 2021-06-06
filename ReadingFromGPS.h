e0722029d79af26a51c83da680e1fcb85e5751d2
3omdawy160 <85120725+3omdawy160@users.noreply.github.com>
2021-06-06 02:37:56 +0200

    Update ReadingFromGPS.h

--- ReadingFromGPS.h
+++ ReadingFromGPS.h
@@ -11,13 +11,13 @@ void readThis1(char *y)
 	int t=0;
 	for(i=0;i<500;i++)
 	{
-		c = UART1_Read();
+		c = UART2_Read();
 		y[i] = c;
 		if(x[i]==',' && x[i-1]=='A' && x[i-2]=='G' && x[i-3]=='G' && x[i-4]=='P' && x[i-5]=='G' && x[i-6]=='$')
 		{
 			for(t=0;t<26;t++)
 			{
-				c = UART1_Read();
+				c = UART2_Read();
 				if(t==23 || t==24 || t==25){y[t-23]=c;}
 			}
 			break;
