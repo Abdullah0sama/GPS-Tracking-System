938c0c7ef5cfc9d03991d009a8c901552538f21e
Ahmedkhaled2221 <85367025+Ahmedkhaled2221@users.noreply.github.com>
2021-06-06 02:02:47 +0200

    add read form uart_DR_R  and fix errors
    errors : using un accessible serial interference port

--- uart4_init.h
+++ uart4_init.h
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 
 void UART_Init(void){
-SYSCTL_RCGCUART_R |=0x0004; //enable uart4 and Port D
+SYSCTL_RCGCUART_R |=0x0004; //enable uart2 and Port D
 SYSCTL_RCGCGPIO_R |=0x0008;
 UART2_CTL_R &=~0x0001; //diable uart uartctl
 
-UART2_IBRD_R = 0x0208; //or 104 
-UART2_FBRD_R = 0x35 ;   //baudrate
+UART2_IBRD_R = 0x0104; //change it from 204 to 104 default or 204 
+UART2_FBRD_R = 0x11 ;   //as we changed value of ibrd from 204 to 104 to change fbrd  baudrate
 UART2_LCRH_R = 0x0070;      
 UART2_CTL_R |= 0x0001;  //enable uartctl
 
@@ -20,8 +20,8 @@ GPIO_PORTD_AMSEL_R &=~ 0XC0;
 
 }
 
-long long UART1_Read(void)
+ uint8_t UART2_Read(void)
 {
-	while((UART1_FR_R & 0x0010) != 0);
-	return (long long)(UART1_DR_R&0xFF);
+	while((UART2_FR_R & 0x0010) != 0);
+	return (uint8_t)(UART2_DR_R&0xFF);
 }
