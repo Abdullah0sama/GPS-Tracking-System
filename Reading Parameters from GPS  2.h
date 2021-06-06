66be48238aa45de9a51c2c7d835e837a73c0dfa0
3omdawy160 <85120725+3omdawy160@users.noreply.github.com>
2021-06-06 02:38:26 +0200

    Update Reading Parameters from GPS  2.h

--- Reading Parameters from GPS  2.h
+++ Reading Parameters from GPS  2.h
@@ -1,5 +1,5 @@
 
-char UART1_Read(void)
+char UART2_Read(void)
 {
 	while((UART1_FR_R & 0x0010) != 0);
 	return (UART1_DR_R&0xFF);
@@ -13,13 +13,13 @@ double readThis2()
 	int t=0;
 	for(i=0;i<500;i++)
 	{
-		c = UART1_Read();
+		c = UART2_Read();
 		x[i] = c;
 		if(x[i]==',' && x[i-1]=='A' && x[i-2]=='G' && x[i-3]=='G' && x[i-4]=='P' && x[i-5]=='G' && x[i-6]=='$')
 		{
 			for(t=0;t<26;t++)
 			{
-				c = UART1_Read();
+				c = UART2_Read();
 				if(t==23 || t==24 || t==25){y[t-23]=c;}
 			}
 			break;
