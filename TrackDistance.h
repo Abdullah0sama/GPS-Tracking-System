017c7fb477ab283593341258b78f6a68fd63809c
ao <abdullahosama15@gmail.com>
2021-06-13 20:39:28 +0200

    Minor changes

--- TrackDistance.h
+++ TrackDistance.h
@@ -1,16 +1,27 @@
 #include "math.h"
-#include "GetPosition.h"
 #include "Switch.h"
+#include "LCD_init.h"
 #include "LED_init.h"
-
+#include  "ReadingFromGPS.h"
+#include  "stdio.h"
 #define     PI 3.14159265359
 
 double deg2rad(double deg){
     return deg * (PI / 180);
 }
-
+double converCoordFormat(double temp){
+    double y = (int) temp / 100;
+    double z = (temp - y * 100) / 60;
+    return y + z;
+}
 // Calculate distance between two points
 double distanceBetweenPoints(pos first, pos second){
+    first.lat = converCoordFormat(first.lat);
+    first.lon = converCoordFormat(first.lon);
+    second.lat = converCoordFormat(second.lat);
+    second.lon = converCoordFormat(second.lon);
+
+
     double radius = 6.371 * 1000000;
     double dLat = deg2rad(second.lat - first.lat);
     double dLon = deg2rad(second.lon - first.lon);
@@ -26,19 +37,19 @@ double distanceBetweenPoints(pos first, pos second){
 // return total distance from moment it is called until the switch is pushed
 double trackDistance(){
     double totalDistance = 0;
-    pos prev = getPosition();
+    pos prev = readThis();
 
     while(!isSwitchPushed()){
 
         //delay for some time 
         // delay();
-        pos currentPos = getPosition();
+        pos currentPos = readThis();
         double distance = distanceBetweenPoints(prev, currentPos);
         if(distance > 1){
             prev = currentPos;
             totalDistance += distance;
+            display_LCD(totalDistance);
         }
-        // display(distance);
     }
     turnOnLED();
     return totalDistance;
