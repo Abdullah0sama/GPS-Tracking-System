463564eb7eced6d8e5ccf526c21955d4d0880889
mohmedali1801534 <85204433+mohmedali1801534@users.noreply.github.com>
2021-06-13 21:20:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -15,39 +15,24 @@ void setup(){
 int main(){
   UART_Init():	
   setup();
+  delay_milli(500);
+  LCD_init();
+  delay_milli(500);
+  LCD_Cmd(0x80);	
+  delay_milli(500);
+  Lcd_String("GPS TRACKING");
+  delay_milli(500);
+  LCD_Cmd(0x02);
+  delay_milli(100);
+  LCD_Cmd(0xC0);
+  delay_milli(100);
+  Lcd_String("Distance=___");	
   trackDistance();
   	
   //sevenSegment_init();
   //delay();
  // delay_sm();
- //display(555); 
-	setupLED();
-	turnOnLED();
-        delay_milli(500);
-        LCD_init();
-        delay_milli(500);
-
-for(;;)
-  {
-     delay_milli(500);
-
-     LCD_Cmd(0x80);
-
-     delay_milli(500);
-     Lcd_String("GPS TRACKING");
-
-     delay_milli(500);
-
-    LCD_Cmd(0x02);
-    delay_milli(100);
-    LCD_Cmd(0xC0);
-    delay_milli(100);
-
-
-    Lcd_String("Distance=___");
-
-  }
-	
+ //display(555); 	
 }	
 	
 	
